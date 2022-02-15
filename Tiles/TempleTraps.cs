ed855bd897b4e586ecef02a37842c6b32c1c5809
MeowLorrrd <jayola300@gmail.com>
2022-02-15 16:52:32 +0100

    nu'in

---
+++ Tiles/TempleTraps.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.Tiles
+{
+    public class TempleTraps : GlobalTile
+    {
+        public override void SetDefaults()
+        {
+            
+        }
+        public override bool CanExplode(int i, int j, int type)
+        {
+            if (type == TileID.Traps)//figure out how to select only temple traps
+            {
+
+            }
+            return base.CanExplode(i, j, type);
+        }
+    }
+}
