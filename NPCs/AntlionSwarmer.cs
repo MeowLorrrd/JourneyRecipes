b84748c506c59bec922f79b363f131f73327a8e3
MeowLorrrd <jayola300@gmail.com>
2022-02-15 21:14:50 +0100

    giant flying antlion 1.4 changes

---
+++ NPCs/AntlionSwarmer.cs
@@ -0,0 +1,27 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class AntlionSwarmer : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.FlyingAntlion)
+            {
+                npc.value = 1950f;
+            }
+        }
+        public override void NPCLoot(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.FlyingAntlion)
+            {
+                if (Main.rand.Next(50) == 0)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, ItemID.AntlionMandible);
+                }
+            }
+        }
+    }
+}
