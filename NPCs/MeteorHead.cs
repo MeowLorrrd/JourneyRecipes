dc545e34e64a85a0c4a370f87c8cabcff092cf70
MeowLorrrd <jayola300@gmail.com>
2022-02-15 16:17:58 +0100

    meteor head 52hp pre-hm 56hp hm

---
+++ NPCs/MeteorHead.cs
@@ -0,0 +1,20 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class MeteorHead : GlobalNPC
+    {
+        public override void ScaleExpertStats(NPC npc, int numPlayers, float bossLifeScale)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.MeteorHead)
+            {
+                if (!Main.hardMode && Main.expertMode)
+                    npc.lifeMax = 52;
+                if (Main.hardMode && Main.expertMode)
+                    npc.lifeMax = 56;
+            }
+        }
+    }
+}
