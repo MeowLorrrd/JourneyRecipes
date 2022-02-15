77ef85609875526ed0641ee613a0dd5ffb85ccaf
MeowLorrrd <jayola300@gmail.com>
2022-02-15 23:30:56 +0100

    bone pick 1/20

---
+++ NPCs/UndeadMiner.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class UndeadMiner : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.UndeadMiner)
+            {
+                if (Main.rand.Next(20) == 0)
+                    Item.NewItem(npc.Hitbox, ItemID.BonePickaxe);
+
+            }
+        }
+    }
+}
