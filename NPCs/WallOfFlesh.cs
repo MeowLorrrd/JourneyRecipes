220a34ec3ce7af608a1e1c12d4c07ed3010db67c
MeowLorrrd <jayola300@gmail.com>
2022-02-16 00:23:52 +0100

    wof not poisoned immune

---
+++ NPCs/WallOfFlesh.cs
@@ -0,0 +1,15 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class WallOfFlesh : GlobalNPC
+    {
+        public override void SetDefaults(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && (npc.type == NPCID.WallofFlesh || npc.type == NPCID.WallofFleshEye))
+                npc.buffImmune[BuffID.Poisoned] = false;
+        }
+    }
+}
