2162bce2845226f2cfa2704ce01813e505895097
MeowLorrrd <jayola300@gmail.com>
2022-02-16 00:37:03 +0100

    made weapon

---
+++ Items/Weapons/Melee/BreathingReed.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.Items.Weapons.Melee
+{
+    public class BreathingReed : GlobalItem
+    {
+        public override void SetDefaults(Item item)
+        {
+            if (Config.Instance.allowWeaponStat && item.type == ItemID.BreathingReed)
+            {
+                item.damage = 10;
+                item.knockBack = 4f;
+                item.useTime = 27;
+                item.useAnimation = 27;
+                item.useStyle = ItemUseStyleID.SwingThrow;
+                item.melee = true;
+            }
+        }
+    }
+}
