903ca039c4948c06ff09704095b8a4a1e354230b
MeowLorrrd <jayola300@gmail.com>
2022-02-15 23:38:46 +0100

    merchant sells sharpening station

---
+++ NPCs/GlobalNPCShop.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class GlobalNPCShop : GlobalNPC
+    {
+        public override void SetupShop(int type, Chest shop, ref int nextSlot)
+        {
+            if (Config.Instance.allowNPCStat && type == NPCID.Merchant && Main.hardMode)
+            {
+                shop.item[nextSlot].SetDefaults(ItemID.SharpeningStation);
+                shop.item[nextSlot].shopCustomPrice = 100000;
+                nextSlot++;
+            }
+        }
+    }
+}
