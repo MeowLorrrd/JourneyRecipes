799417d91dcd7804a9b3d95bb1e6099a9d62fa30
MeowLorrrd <jayola300@gmail.com>
2022-02-15 16:45:52 +0100

    meteorite cannot be boomed before wof

---
+++ Tiles/Meteorite.cs
@@ -0,0 +1,16 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.Tiles
+{
+    public class Meteorite : GlobalTile
+    {
+        public override bool CanExplode(int i, int j, int type)
+        {
+            if (type == TileID.Meteorite && !Main.hardMode)
+                return false;
+            return base.CanExplode(i, j, type);
+        }
+    }
+}
