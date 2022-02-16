0fefb0e06846e52bb95e2057cd8333adaea1f558
MeowLorrrd <jayola300@gmail.com>
2022-02-16 20:19:40 +0100

    rod of dixcord 1/400

---
+++ NPCs/ChaosElemental.cs
@@ -0,0 +1,19 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace JourneyRecipes.NPCs
+{
+    public class ChaosElemental : GlobalNPC
+    {
+        public override void NPCLoot(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.ChaosElemental && Main.expertMode)
+            {
+                int i = Main.rand.Next(400);
+                if (i == 0)
+                    Item.NewItem(npc.Hitbox, ItemID.RodofDiscord);
+            }
+        }
+    }
+}
