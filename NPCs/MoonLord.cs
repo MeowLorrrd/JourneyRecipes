5ad0e74cf5a9668ea7c32bc6a1d9eef88294e7ac
MeowLorrrd <jayola300@gmail.com>
2022-02-18 08:05:03 +0100

    drops 1 plat (2.5x in expert) drops super heals, still drops greater heals (fix that please thankyou)

--- NPCs/MoonLord.cs
+++ NPCs/MoonLord.cs
@@ -2,16 +2,31 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
+
 namespace JourneyRecipes.NPCs
 {
     public class MoonLord : GlobalNPC
     {
         public override void NPCLoot(NPC npc)
         {
-            if (Config.Instance.allowNPCStat && npc.type == NPCID.MoonLordCore && !Main.expertMode)
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.MoonLordCore)
+            {
+                Item.NewItem(npc.Hitbox, ItemID.SuperHealingPotion, Main.rand.Next(5, 15));
+                //
+            }
+        }
+        public override void SetDefaults(NPC npc)
+        {
+            if (Config.Instance.allowNPCStat && npc.type == NPCID.MoonLordCore)
+                npc.value = 1000000;
+        }
+        public override bool PreNPCLoot(NPC npc)
+        {
+            if (npc.type == NPCID.MoonLordCore)
             {
 
             }
+            return base.PreNPCLoot(npc);
         }
     }
 }
