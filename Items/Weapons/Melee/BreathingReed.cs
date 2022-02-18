3a9269b8124f1629a526317a81cd9b51c267327b
MeowLorrrd <jayola300@gmail.com>
2022-02-18 15:54:34 +0100

    prefix support

--- Items/Weapons/Melee/BreathingReed.cs
+++ Items/Weapons/Melee/BreathingReed.cs
@@ -1,6 +1,7 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Terraria.Utilities;
 
 namespace JourneyRecipes.Items.Weapons.Melee
 {
@@ -17,6 +18,14 @@ namespace JourneyRecipes.Items.Weapons.Melee
                 item.useStyle = ItemUseStyleID.SwingThrow;
                 item.melee = true;
             }
+        }
+        public override int ChoosePrefix(Item item, UnifiedRandom rand)
+        {
+            if (Config.Instance.allowWeaponStat && item.type == ItemID.BreathingReed)
+            {
+                return rand.Next(new int[] { PrefixID.Legendary, PrefixID.Godly, PrefixID.Savage, PrefixID.Superior, PrefixID.Demonic, PrefixID.Deadly, PrefixID.Unpleasant, PrefixID.Murderous, PrefixID.Massive, PrefixID.Agile, PrefixID.Forceful, PrefixID.Sharp, PrefixID.Strong, PrefixID.Dangerous, PrefixID.Large, PrefixID.Zealous, PrefixID.Hurtful, PrefixID.Pointy, PrefixID.Quick, PrefixID.Nasty, PrefixID.Bulky, PrefixID.Ruthless, PrefixID.Keen, PrefixID.Nimble, PrefixID.Light, PrefixID.Heavy, PrefixID.Lazy, PrefixID.Small, PrefixID.Slow, PrefixID.Damaged, PrefixID.Dull, PrefixID.Tiny, PrefixID.Weak, PrefixID.Sluggish, PrefixID.Shameful, PrefixID.Shoddy, PrefixID.Unhappy, PrefixID.Annoying, PrefixID.Terrible, PrefixID.Broken });
+            }
+            return base.ChoosePrefix(item, rand);
         }
     }
 }
