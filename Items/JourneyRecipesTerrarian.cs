e778bd3c71d1973782c6d1573cb248b9f1add2e4
MeowLorrrd <jayola300@gmail.com>
2022-06-09 20:06:18 +0200

    notes

--- Items/JourneyRecipesTerrarian.cs
+++ Items/JourneyRecipesTerrarian.cs
@@ -15,14 +15,14 @@ namespace JourneyRecipes.Items
         public override int ChoosePrefix(Item item, UnifiedRandom rand)
         {
             int[] prefix = new int[] { PrefixID.Keen, PrefixID.Superior, PrefixID.Forceful, PrefixID.Broken, PrefixID.Damaged, PrefixID.Shoddy, PrefixID.Hurtful, PrefixID.Strong, PrefixID.Unpleasant, PrefixID.Weak, PrefixID.Ruthless, PrefixID.Godly, PrefixID.Demonic, PrefixID.Zealous };
-            //listed prefix ids
+            //vanilla prefix ids
             if (item.type == ItemID.Terrarian && Config.Instance.allowWeaponStat)
             {
                 if (Main.rand.NextBool(15))//chance 1/15
                 {
                     return mod.PrefixType(rand.NextBool(1) ? "Legendary" : "");//select this prefix
                 }
-                else//chance 1/14
+                else//chance 14/15
                 {
                     return prefix[Main.rand.Next(0, 14)];//select one of the prefixes from above
                 }
