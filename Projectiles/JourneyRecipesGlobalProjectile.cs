13a377661d9e00f242acc7064c4a9931126bb89e
MeowLorrrd <jayola300@gmail.com>
2022-06-11 17:13:08 +0200

    else if >> switch

--- Projectiles/JourneyRecipesGlobalProjectile.cs
+++ Projectiles/JourneyRecipesGlobalProjectile.cs
@@ -11,60 +11,87 @@ namespace JourneyRecipes.Projectiles
         }
         public override void SetDefaults(Projectile projectile)
         {
-            switch (projectile.type)
+            if (projectile.type == 92)
             {
-                case 92:
-                    projectile.damage = 7;
-                    projectile.penetrate = 1;
-                    break;
-                case 242:
-                    projectile.penetrate = 3;
-                    break;
-                case 260:
-                    projectile.penetrate = 1;
-                    break;
-                case 266:
-                    projectile.netImportant = true;
-                    projectile.usesIDStaticNPCImmunity = true;
-                    projectile.idStaticNPCHitCooldown = 12;
-                    break;
-                case 297:
-                    projectile.penetrate = 3;
-                    break;
-                case 387:
-                    projectile.netImportant = true;
-                    projectile.usesIDStaticNPCImmunity = true;
-                    projectile.idStaticNPCHitCooldown = 16;
-                    break;
-                case 388:
-                    projectile.netImportant = true;
-                    projectile.usesIDStaticNPCImmunity = true;
-                    projectile.idStaticNPCHitCooldown = 12;
-                    break;
-                case 389:
-                    projectile.usesIDStaticNPCImmunity = true;
-                    projectile.idStaticNPCHitCooldown = 12;
-                    projectile.extraUpdates = 2;
-                    break;
-                case 433:
-                    if (projectile.melee)
-                    {
-                        projectile.damage = 70;
-                    }
-                    break;
-                case 533:
-                    projectile.netImportant = true;
-                    projectile.usesIDStaticNPCImmunity = true;
-                    projectile.idStaticNPCHitCooldown = 8;
-                    break;
-                case 697:
-                    projectile.scale = 1.35f;
-                    projectile.damage = 150;
-                    break;
-                case 708:
-                case 709:
-                    projectile.damage = 70;
-                    break;
+                projectile.damage = 7;
+                projectile.penetrate = 1;
+            }
+            else if (projectile.type == 242)
+            {
+                projectile.penetrate = 3;
+                projectile.extraUpdates = 7;
+                projectile.usesLocalNPCImmunity = true;
+                projectile.localNPCHitCooldown = 15;
+            }
+            else if (projectile.type == 260)
+            {
+                projectile.penetrate = 1;
+            }
+            else if (projectile.type == 266)
+            {
+                projectile.netImportant = true;
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 12;
+            }
+            else if (projectile.type == 297)
+            {
+                projectile.penetrate = 3;
+            }
+            else if (projectile.type == 390 || projectile.type == 391 || projectile.type == 392)
+            {
+                projectile.minionSlots = 1f;
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 15;
+            }
+            else if (projectile.type == 387)
+            {
+                projectile.netImportant = true;
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 16;
+            }
+            else if (projectile.type == 388)
+            {
+                projectile.netImportant = true;
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 12;
+            }
+            else if (projectile.type == 389)
+            {
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 12;
+                projectile.extraUpdates = 2;
+            }
+            else if (projectile.type == 433)
+            {
+                //TODO: MODIFY DAMAGE
+            }
+            else if (projectile.type == 533)
+            {
+                projectile.netImportant = true;
+                projectile.usesIDStaticNPCImmunity = true;
+                projectile.idStaticNPCHitCooldown = 8;
+            }
+            else if (projectile.type == 697)
+            {
+                projectile.scale = 1.35f;
+                projectile.damage = 150;
+                projectile.ownerHitCheck = true;
+                projectile.usesLocalNPCImmunity = true;
+                projectile.localNPCHitCooldown = 12;
+            }
+            else if (projectile.type == 708)
+            {
+                projectile.usesLocalNPCImmunity = true;
+                projectile.localNPCHitCooldown = 6;
+                projectile.ownerHitCheck = true;
+                //TODO
+            }
+            else if (projectile.type == 709)
+            {
+                projectile.extraUpdates = 1;
+                projectile.noEnchantments = true;
+                projectile.scale = 0.6f;
+                //TODO
             }
         }
     }
