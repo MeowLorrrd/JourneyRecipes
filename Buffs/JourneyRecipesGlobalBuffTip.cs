10576f25997efe347473e528a37700f9ca93fd80
MeowLorrrd <jayola300@gmail.com>
2022-06-12 12:57:01 +0200

    re-added buff tip for Striking Moment & Fishing

---
+++ Buffs/JourneyRecipesGlobalBuffTip.cs
@@ -0,0 +1,20 @@
+﻿using Terraria.ModLoader;
+using Terraria.Localization;
+
+namespace JourneyRecipes.Buffs
+{
+    public class JourneyRecipesGlobalBuffTip : GlobalBuff
+    {
+        public override void ModifyBuffTip(int type, ref string tip, ref int rare)
+        {
+            if (type == 198)
+            {
+                tip = Language.GetTextValue("Mods.JourneyRecipes.BuffTip.ParryDamageBuff");
+            }
+            else if (type == 121)
+            {
+                tip = Language.GetTextValue("Mods.JourneyRecipes.BuffTip.Fishing");
+            }
+        }
+    }
+}
