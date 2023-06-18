33cd37943df4b2dd7f272b65b9f4bb72d48cdc89
MeowLorrrd <82777045+MeowLorrrd@users.noreply.github.com>
2023-06-18 18:29:25 +0200

    else if im that much of n idiot...

--- Items/JourneyRecipesGlobalItemAccessory.cs
+++ Items/JourneyRecipesGlobalItemAccessory.cs
@@ -7,21 +7,19 @@ namespace JourneyRecipes.Items
 {
     public class JourneyRecipesGlobalItemAccessory : GlobalItem
     {
-        public override bool Autoload(ref string name)
-        {
-            return GetInstance<JourneyRecipesServerConfig>().allowAccessoryStat;
-        }
+        public override bool Autoload(ref string name) => return GetInstance<JourneyRecipesServerConfig>().allowAccessoryStat;
+        
         public override void SetDefaults(Item item)
         {
             if (item.type == 49 || item.type == 860)
             {
                 item.lifeRegen = 2;
             }
-            if (item.type == 3016)
+            else if (item.type == 3016)
             {
                 item.defense = 8;
             }
-            if (item.type== 3245)
+            else if (item.type== 3245)
             {
                 item.accessory = true;
                 item.damage = 0;
@@ -31,7 +29,7 @@ namespace JourneyRecipes.Items
                 item.useAnimation = 0;
                 item.useStyle = 0;
             }
-            if (item.type == 154)
+            else if (item.type == 154)
             {
                 item.notAmmo = true;
             }
@@ -42,75 +40,75 @@ namespace JourneyRecipes.Items
             {
                 player.GetModPlayer<JourneyRecipesPlayer>().autoReuseGlove = true;
             }
-            if (item.type == 897)
+            else if (item.type == 897)
             {
                 player.GetModPlayer<JourneyRecipesPlayer>().autoReuseGlove = true;
             }
-            if (item.type == 908)
+            else if (item.type == 908)
             {
                 player.lavaRose = true;
             }
-            if (item.type == 936)
+            else if (item.type == 936)
             {
                 player.GetModPlayer<JourneyRecipesPlayer>().autoReuseGlove = true;
             }
-            if (item.type == 1343)
+            else if (item.type == 1343)
             {
                 player.meleeDamage += .02f;
                 player.meleeSpeed += .02f;
                 player.GetModPlayer<JourneyRecipesPlayer>().autoReuseGlove = true;
             }
-            if (item.type == 2221)
+            else if (item.type == 2221)
             {
                 player.statManaMax2 += 20;
             }
-            if (item.type == 532 || item.type == 862 || item.type == 1247)
+            else if (item.type == 532 || item.type == 862 || item.type == 1247)
             {
                 player.starCloak = false;
                 player.GetModPlayer<JourneyRecipesPlayer>().StarCloak = true;
             }
-            if (item.type == 1303)
+           else if (item.type == 1303)
             {
                 if (!player.wet)
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 0.225f, 0.05f, 0.15f);
                 }
-                if (player.wet)
+                else
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 1.8f, 0.4f, 1.2f);
                 }
             }
-            if (item.type == 1860)
+            else if (item.type == 1860)
             {
                 if (!player.wet)
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 0.225f, 0.05f, 0.15f);
                 }
-                if (player.wet)
+                else
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 1.8f, 0.4f, 1.2f);
                 }
             }
-            if (item.type == 1861)
+            else if (item.type == 1861)
             {
                 if (!player.wet)
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 0.05f, 0.15f, 0.225f);
                 }
-                if (player.wet)
+                else 
                 {
                     Lighting.AddLight((int)player.Center.X / 16, (int)player.Center.Y / 16, 0.4f, 1.2f, 1.8f);
                 }
             }
-            if (item.type == 3245)
+            else if (item.type == 3245)
             {
                 player.GetModPlayer<JourneyRecipesPlayer>().BoneGloveItem = item;
             }
-            if (player.bee)
+            else if (player.bee)
             {
                 player.GetModPlayer<JourneyRecipesPlayer>().BeeBeeBee = true;
             }
-            if (item.type == 1131)
+            else if (item.type == 1131)
             {
                 player.gravControl = true;
             }
